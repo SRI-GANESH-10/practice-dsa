4522fb78e12fcba40d982da678a2850d2697b8f2
Sri Ganesh <sri.ganesh@privasapien.com>
2026-05-19 23:10:43 +0530

    Add implementation for finding consecutive ones in an array

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -20,3 +20,25 @@
 // }
 // missingNumberUsingXor([1,2,3,4,5,7,8,9])
 
+//! Find consecutive ones in an array
+
+// function consecutiveOnes(arr){
+//     let cnt = 0;
+//     let max = 0;
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]==1){
+//             cnt++;
+//             if(cnt > max){
+//                 max = cnt;
+//             }
+//         }
+//         else{
+//             cnt = 0;
+//         }
+//     }
+//     console.log("Number of consective one's are : " + max);
+// }
+
+// consecutiveOnes([1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,0])
+
+

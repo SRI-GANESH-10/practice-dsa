54f25c32c7efb2bba6addb9d2a7d952909c3bec2
Sri Ganesh <sri.ganesh@privasapien.com>
2026-05-30 14:28:23 +0530

    Add implementation for sorting an array of 0's, 1's, and 2's using Dutch National Flag Algorithm

--- arrays/arrayNotes.js
+++
@@ -1,6 +0,0 @@
-//! PATTERNS : 
-
-//? 1. Two Pointer Pattern:
-    //* This pattern is used when we have to find pairs in an array that satisfy a certain condition.
-    //* We can reduce the time cmplexity O(n^2) -> O(n);
-    //* mainly in sorted arrays

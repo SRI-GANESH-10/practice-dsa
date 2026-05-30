4ef4ed60edf79fea535fb3f2f5bb695fb9d6068a
Sri Ganesh <sri.ganesh@privasapien.com>
2026-05-30 13:21:30 +0530

    Update arrayNotes.js to enhance Two Pointer Pattern description and remove outdated Two Sum Problem notes

--- arrays/arrayNotes.js
+++ arrays/arrayNotes.js
@@ -1,4 +1,6 @@
-//! Two Sum Problem:
-//? One Approach we can have is sorting and going from left and right which is usefull for yes or no ones;
-//? Another is Hashset and going in a loop once and checking if the diff is there in map or not - use full for indices;
+//! PATTERNS : 
 
+//? 1. Two Pointer Pattern:
+    //* This pattern is used when we have to find pairs in an array that satisfy a certain condition.
+    //* We can reduce the time cmplexity O(n^2) -> O(n);
+    //* mainly in sorted arrays

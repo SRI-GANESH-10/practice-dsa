bceccf21acc5c384fe7aff2ba17dcc7b541e1f05
Sri Ganesh <sri.ganesh@privasapien.com>
2026-08-22 16:17:01 +0530

    Add implementation for finding triplets whose sum is equal to 0 in an array (3 Sum Problem)

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -681,3 +681,34 @@
 //     return res
 // }
 // console.log(majorityElementNBy3([2,2,1,2,1,1,3,1,2]))
+
+//! Find the triplets whose sum is equal to 0 in an array - 3 Sum Problem
+
+// const threeSumProblem = (arr) => {
+//     arr.sort((a , b)=>a-b);
+//     let res = []
+//     for(let i=0;i<arr.length;i++){
+//         if(i>0 && arr[i] === arr[i-1]) continue;
+//         let j = i + 1;
+//         let k = arr.length -1;
+//         while(j<k){
+//             let sum = arr[i] + arr[j] + arr[k];
+
+//             if(sum>0){
+//                 k--;
+//             }
+//             else if(sum <0){
+//                 j++;
+//             }
+//             else{
+//                 res.push([arr[i] , arr[j] , arr[k]])
+//                 j++;
+//                 k--;
+//                 while(j<k && arr[j] === arr[j+1]) j++;
+//                 while(j<k && arr[k] === arr[k-1]) k--;
+//             }
+//         }
+//     }
+//     return res
+// }
+// console.log(threeSumProblem([-2,1,0,2,2,1,0,-4,-1,-3]))
